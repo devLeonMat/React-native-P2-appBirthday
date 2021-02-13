599cf01a6e38e783c09f1f1a86eab52746f89d50
leon matias roslin <leonmatias1991@gmail.com>
2021-02-13 00:33:06 -0500

    Agregando proyecto

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+
+export default function App() {
+
+  return (
+      <SafeAreaView>
+        <Text>Leon</Text>
+      </SafeAreaView>
+  )
+
+}
